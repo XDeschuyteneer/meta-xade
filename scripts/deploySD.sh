9969e2ed4ef0757e919ee0b1f17b0f0645922078
Xavier Deschuyteneer <xdr@ewon.biz>
2018-02-14 09:12:30 +0100

    improve deploySD.sh script

--- scripts/deploySD.sh
+++ scripts/deploySD.sh
@@ -8,5 +8,8 @@ mkdir ${BOOTFS} -p
 mkdir ${ROOTFS} -p
 sudo mount ${SDCARD}p1 ${BOOTFS}
 sudo mount ${SDCARD}p2 ${ROOTFS}
-sudo tar xz ${BUILDDIR}/tmp/deploy/images/raspberrypi3/xadeos-debug-raspberrypi3.tar.xz -C ${ROOTFS}
-sudo cp ${BUILDDIR}/tmp/deploy/images/raspberrypi3/{bcm2710-rpi-3-b.dtb,}
+sudo tar xf ${BUILDDIR}/tmp/deploy/images/raspberrypi3/xadeos-debug-raspberrypi3.tar.xz -C ${ROOTFS}
+sudo cp ${BUILDDIR}/tmp/deploy/images/raspberrypi3/bcm2710-rpi-3-b.dtb ${BOOTFS}/dtb
+sudo cp ${BUILDDIR}/tmp/deploy/images/raspberrypi3/{config.txt, cmdline.txt, bootcode.bin} ${BOOTFS}/
+sudo umount ${ROOTFS}
+sudo umount ${BOOTFS}
